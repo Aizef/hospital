6cef14b3e59fdda13364d96e0d56b3c7a1f0725c
teyken10 <platonovmale@gmail.com>
2025-04-16 13:47:34 +0300

    add errors

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from data.error_handlers import not_found, not_found_2, not_found_3
+from flask import Flask, abort
+from data.error_handlers import *
 from data import db_session
 from data.models.users import User
 from db.db_fill import db_fill
@@ -23,9 +23,46 @@ def load_user(user_id):
 
 
 def main():
+    app.register_error_handler(400, bad_request)
+    app.register_error_handler(401, unauthorized)
+    # app.register_error_handler(402, payment_required)  # flask не поддерживает эту ошибку
+    app.register_error_handler(403, forbidden)
     app.register_error_handler(404, not_found)
-    app.register_error_handler(403, not_found_2)
-    app.register_error_handler(401, not_found_3)
+    app.register_error_handler(405, method_not_allowed)
+    app.register_error_handler(406, not_acceptable)
+    # app.register_error_handler(407, proxy_auth_required)  # flask не поддерживает эту ошибку
+    app.register_error_handler(408, request_timeout)
+    app.register_error_handler(409, conflict)
+    app.register_error_handler(410, gone)
+    app.register_error_handler(411, length_required)
+    app.register_error_handler(412, precondition_failed)
+    app.register_error_handler(413, payload_too_large)
+    app.register_error_handler(414, uri_too_long)
+    app.register_error_handler(415, unsupported_media_type)
+    app.register_error_handler(416, range_not_satisfiable)
+    app.register_error_handler(417, expectation_failed)
+    app.register_error_handler(418, im_a_teapot)
+    app.register_error_handler(421, misdirected_request)
+    app.register_error_handler(422, unprocessable_entity)
+    app.register_error_handler(423, locked)
+    app.register_error_handler(424, failed_dependency)
+    # app.register_error_handler(425, too_early)  # flask не поддерживает эту ошибку
+    # app.register_error_handler(426, upgrade_required)  # flask не поддерживает эту ошибку
+    app.register_error_handler(428, precondition_required)
+    app.register_error_handler(429, too_many_requests)
+    app.register_error_handler(431, headers_too_large)
+    app.register_error_handler(451, legal_unavailable)
+    app.register_error_handler(500, internal_error)
+    app.register_error_handler(501, not_implemented)
+    app.register_error_handler(502, bad_gateway)
+    app.register_error_handler(503, service_unavailable)
+    app.register_error_handler(504, gateway_timeout)
+    app.register_error_handler(505, http_version_not_supported)
+    # app.register_error_handler(506, variant_negotiates)  # flask не поддерживает эту ошибку
+    # app.register_error_handler(507, insufficient_storage)  # flask не поддерживает эту ошибку
+    # app.register_error_handler(508, loop_detected)  # flask не поддерживает эту ошибку
+    # app.register_error_handler(510, not_extended)  # flask не поддерживает эту ошибку
+    # app.register_error_handler(511, network_auth_required)  # flask не поддерживает эту ошибку
     db_session.global_init("db/database.db")
     db_fill(db_session)
     app.register_blueprint(patient_api.blueprint)
--- data/error_handlers.py
+++ data/error_handlers.py
@@ -1,13 +1,122 @@
 from flask import render_template
 
 
+def bad_request(e):
+    return render_template("errors/400.html")
+
+def unauthorized(e):
+    return render_template("errors/401.html")
+
+def payment_required(e):
+    return render_template("errors/402.html")
+
+def forbidden(e):
+    return render_template("errors/403.html")
+
 def not_found(e):
     return render_template("errors/404.html")
 
+def method_not_allowed(e):
+    return render_template("errors/405.html")
 
-def not_found_2(e):
-    return render_template("errors/403.html")
+def not_acceptable(e):
+    return render_template("errors/406.html")
 
+def proxy_auth_required(e):
+    return render_template("errors/407.html")
 
-def not_found_3(e):
-    return render_template("errors/401.html")
+def request_timeout(e):
+    return render_template("errors/408.html")
+
+def conflict(e):
+    return render_template("errors/409.html")
+
+def gone(e):
+    return render_template("errors/410.html")
+
+def length_required(e):
+    return render_template("errors/411.html")
+
+def precondition_failed(e):
+    return render_template("errors/412.html")
+
+def payload_too_large(e):
+    return render_template("errors/413.html")
+
+def uri_too_long(e):
+    return render_template("errors/414.html")
+
+def unsupported_media_type(e):
+    return render_template("errors/415.html")
+
+def range_not_satisfiable(e):
+    return render_template("errors/416.html")
+
+def expectation_failed(e):
+    return render_template("errors/417.html")
+
+def im_a_teapot(e):
+    return render_template("errors/418.html")
+
+def misdirected_request(e):
+    return render_template("errors/421.html")
+
+def unprocessable_entity(e):
+    return render_template("errors/422.html")
+
+def locked(e):
+    return render_template("errors/423.html")
+
+def failed_dependency(e):
+    return render_template("errors/424.html")
+
+def too_early(e):
+    return render_template("errors/425.html")
+
+def upgrade_required(e):
+    return render_template("errors/426.html")
+
+def precondition_required(e):
+    return render_template("errors/428.html")
+
+def too_many_requests(e):
+    return render_template("errors/429.html")
+
+def headers_too_large(e):
+    return render_template("errors/431.html")
+
+def legal_unavailable(e):
+    return render_template("errors/451.html")
+
+def internal_error(e):
+    return render_template("errors/500.html")
+
+def not_implemented(e):
+    return render_template("errors/501.html")
+
+def bad_gateway(e):
+    return render_template("errors/502.html")
+
+def service_unavailable(e):
+    return render_template("errors/503.html")
+
+def gateway_timeout(e):
+    return render_template("errors/504.html")
+
+def http_version_not_supported(e):
+    return render_template("errors/505.html")
+
+def variant_negotiates(e):
+    return render_template("errors/506.html")
+
+def insufficient_storage(e):
+    return render_template("errors/507.html")
+
+def loop_detected(e):
+    return render_template("errors/508.html")
+
+def not_extended(e):
+    return render_template("errors/510.html")
+
+def network_auth_required(e):
+    return render_template("errors/511.html")
